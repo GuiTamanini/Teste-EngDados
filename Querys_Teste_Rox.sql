c5f5589011c7c63579ffc7ab6b060b6e6e4ab058
GuiTamanini <guitamanini93@hotmail.com>
2021-09-02 09:54:10 -0300

    Query 3 Update
    Simplified query 3 (Customer List) coding for better performance

--- Querys_Teste_Rox.sql
+++ Querys_Teste_Rox.sql
@@ -1,53 +1,51 @@
 -- Query 1 - Contagem total de linhas de SalesOrderDetail com mais de 3 linhas de detalhes
 Select count(1) as number_rows from (
 	Select SalesOrderID
-	From [Sales.SalesOrderDetail] 
+	From `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderDetail` 
 	Group By SalesOrderID
 	having count(SalesOrderDetailID) > 3
 ) t
 
-
 -- Query 2 - Join entre as tabelas [Sales.SalesOrderDetail], [Sales.SpecialOfferProduct] e [Production.Product] retornando os 3 produtos mais vendidos
-Select Top (3) PROD.Name
-From [Sales.SalesOrderDetail] as SOD
-Inner Join [Production.Product] as PROD on PROD.ProductID = SOD.ProductID
-Inner Join [Sales.SpecialOfferProduct] as SOP on SOP.ProductID = SOD.ProductID
+Select PROD.Name
+From `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderDetail` as SOD
+Inner Join `teste-roxpartner.bicyclesales_roxpartner.Production_Product` as PROD on PROD.ProductID = SOD.ProductID
+Inner Join `teste-roxpartner.bicyclesales_roxpartner.Sales_SpecialOfferProduct` as SOP on SOP.ProductID = SOD.ProductID
 Group by PROD.DaysToManufacture, PROD.Name
 Order by sum(cast(OrderQty as int)) desc
+LIMIT 3
 
 -- Query 3 - Join entre as tabelas [Person.Person], [Sales.Consumer] e [SalesOrderHeader] para obter nome de clientes e número de pedidos
-Select C.CustomerID 
-      ,(Select count(CustomerID) from [Sales.SalesOrderHeader] as H where H.CustomerID = C.CustomerID group by CustomerID) as num_orders
-	  ,(Select sum(Cast(H.TotalDue as Numeric(10,2))) from [Sales.SalesOrderHeader] as H where H.CustomerID = C.CustomerID group by CustomerID) as tot_price
-into #TotalOrdersTemp 
-From [Sales.Customer] as C
-
+BEGIN
+    CREATE TEMP TABLE TotalOrdersTemp
+    AS
+    Select CustomerID, count(customerID) as num_orders, sum(totaldue) as tot_price
+	From `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderHeader`
+	Group by customerID
+;
 Select C.CustomerID
 	   ,P.FirstName
 	   ,P.MiddleName
 	   ,P.LastName
 	   ,T.num_orders
 	   ,T.tot_price
-From [Person.Person] as P
-Inner Join [Sales.Customer] as C on C.PersonID = P.BusinessEntityID
-Inner Join [Sales.SalesOrderHeader] as SOH on SOH.CustomerID = C.CustomerID
-Inner Join #TotalOrdersTemp as T on T.CustomerID = C.CustomerID
+From `teste-roxpartner.bicyclesales_roxpartner.Person_Person` as P
+Inner Join `teste-roxpartner.bicyclesales_roxpartner.Sales_Customer` as C on C.PersonID = P.BusinessEntityID
+Inner Join `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderHeader` as SOH on SOH.CustomerID = C.CustomerID
+Inner Join TotalOrdersTemp as T on T.CustomerID = C.CustomerID
 Order By T.num_orders, T.tot_price desc
-
-Drop Table #TotalOrdersTemp
+;END
 
 -- Query 4 - Join entre as tabelas [Sales.SalesOrderDetail], [SalesOrderHeader] e [Production.Product] para obter o total de produtos por ProductID e OrderDate
-
 Select SOD.ProductID, sum(cast(OrderQty as int)) as Tot_Prod_byID_Date
-From [Sales.SalesOrderDetail] as SOD
-Inner Join [Sales.SalesOrderHeader] as SOH on SOH.SalesOrderID = SOD.SalesOrderID
-Inner Join [Production.Product] as PROD on PROD.ProductID = SOD.ProductID
+From `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderDetail` as SOD
+Inner Join `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderHeader` as SOH on SOH.SalesOrderID = SOD.SalesOrderID
+Inner Join `teste-roxpartner.bicyclesales_roxpartner.Production_Product` as PROD on PROD.ProductID = SOD.ProductID
 Group by SOD.ProductID, SOH.OrderDate
 
 -- Query 5 - Retornar campos SalesOrderID, OrderDate e TotalDue com data de setembro/2011 e valor maior que 1000
-
 Select SalesOrderID, OrderDate, TotalDue 
-From [Sales.SalesOrderHeader]
-Where Convert(DATETIME,OrderDate,103) between '01/09/2011' and '30/09/2011' 
+From `teste-roxpartner.bicyclesales_roxpartner.Sales_SalesOrderHeader`
+Where OrderDate between '2011-09-01' and '2011-09-30' 
 	And TotalDue > 1000
 Order by TotalDue desc
